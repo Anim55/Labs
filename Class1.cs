59690f6ad2ccea06aa88ee24b575bc4c1047a9e9
Anim55 <31664158+Anim55@users.noreply.github.com>
2017-10-18 08:35:56 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Lab3
+{
+    class AirLines
+    {
+        readonly int id;
+        public const string func = "Fly";
+        public string destination { get; set;}
+        public int? number { get; set; }
+        public string day { get; set; }
+        public int type { get; set; }
+        public string time { get;}
+        static int counter;
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        public string GetDay()
+        {
+            return (this.day);
+        }
+        public string GetDestination()
+        {
+            return (this.destination);
+        }
+
+
+        public override bool Equals(object obj)
+        {
+            Console.WriteLine("Object equalified? \n" + base.Equals(obj));
+            return base.Equals(obj);
+        }
+
+
+
+        public AirLines(int type, int number, string day, string time, string destination)
+        {
+            ++counter;
+            this.type = type;
+            this.number = number;
+            this.day = day;
+            this.time = time;
+            this.destination = destination;
+        }
+        public AirLines(int id)
+        {
+            ++counter;
+            this.number = null;
+        }
+        public AirLines()
+        {
+            ++counter;
+        }
+        public void DestDay(string destination, string day)
+        {
+            this.destination = destination;
+            this.day = day;
+        }
+        static AirLines()
+        {
+
+        }
+        protected AirLines(string time)
+        {
+            ++counter;
+        }
+        private void Func1(ref string str1)
+        {
+            if (str1 == null)
+            {
+                str1 = "some";
+            }
+        }
+        public void Input(int type, int number, string day, string destination)
+        {
+            this.type = type;
+            this.number = number;
+            this.day = day;
+            this.destination = destination;
+        }
+        private void Func2(out string str2)
+        {
+            str2 = "Jeff";
+        }
+        public virtual void About()
+        {
+            Console.Write(type +"\t");
+            Console.Write(number + "\t");
+            Console.Write(day + "\t");
+            Console.Write(destination + "\n");
+        }
+    }
+    partial class AirLine
+    {
+        public string destination { get; set; }
+        public int? number { get; set; }
+        public string day { get; set; }
+        public int type { get; set; }
+    }    
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AirLines AirLinesMM = new AirLines();
+            AirLines AirLinesML = new AirLines(2, 13, "Monday", "23:00", "London");
+            AirLines AirLinesWM = new AirLines(4);
+            AirLines AirLinesFB = new AirLines();
+            AirLines AirLinesSM = new AirLines();
+            AirLinesMM.Input(1, 23, "Monday", "Moskow");
+            AirLinesWM.Input(1, 11, "Wednesday", "Moskow");
+            AirLinesFB.Input(3, 15, "Friday", "Berlin");
+            AirLinesSM.Input(2, 12, "Sunday", "Moskow");
+            if(AirLinesSM is AirLines)
+            {
+                Console.Write("Object is AirLine \n");
+            }
+            AirLinesSM.Equals(AirLinesFB);
+            AirLines[] Poletaem = { AirLinesMM, AirLinesML, AirLinesWM, AirLinesFB, AirLinesSM };
+            foreach (AirLines i in Poletaem)
+            {
+                if(i.GetDay().Equals("Monday"))
+                {
+                    i.About();
+                }
+            }
+            Console.WriteLine();
+            foreach (AirLines i in Poletaem)
+            {
+                if (i.GetDestination().Equals("Moskow"))
+                {
+                    i.About();
+                }
+            }
+        }
+    }
+}

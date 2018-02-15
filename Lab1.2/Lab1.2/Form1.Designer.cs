00051c175971aebf79715fdd4c106719c730ef19
Anim55 <31664158+Anim55@users.noreply.github.com>
2018-02-15 08:14:18 +0300

    Add files via upload

---
+++ Lab1.2/Lab1.2/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Lab1._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.studentsLabel = new System.Windows.Forms.Label();
+            this.numberOfStuents = new System.Windows.Forms.TextBox();
+            this.generate = new System.Windows.Forms.Button();
+            this.collectionBox = new System.Windows.Forms.RichTextBox();
+            this.minSort = new System.Windows.Forms.Button();
+            this.maxSort = new System.Windows.Forms.Button();
+            this.linqBox = new System.Windows.Forms.RichTextBox();
+            this.query1 = new System.Windows.Forms.Button();
+            this.query2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // studentsLabel
+            // 
+            this.studentsLabel.AutoSize = true;
+            this.studentsLabel.Location = new System.Drawing.Point(13, 13);
+            this.studentsLabel.Name = "studentsLabel";
+            this.studentsLabel.Size = new System.Drawing.Size(102, 13);
+            this.studentsLabel.TabIndex = 0;
+            this.studentsLabel.Text = "Number of students:";
+            // 
+            // numberOfStuents
+            // 
+            this.numberOfStuents.Location = new System.Drawing.Point(121, 10);
+            this.numberOfStuents.Name = "numberOfStuents";
+            this.numberOfStuents.Size = new System.Drawing.Size(22, 20);
+            this.numberOfStuents.TabIndex = 1;
+            // 
+            // generate
+            // 
+            this.generate.Location = new System.Drawing.Point(158, 7);
+            this.generate.Name = "generate";
+            this.generate.Size = new System.Drawing.Size(75, 23);
+            this.generate.TabIndex = 2;
+            this.generate.Text = "Show Marks";
+            this.generate.UseVisualStyleBackColor = true;
+            this.generate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // collectionBox
+            // 
+            this.collectionBox.Location = new System.Drawing.Point(12, 46);
+            this.collectionBox.Name = "collectionBox";
+            this.collectionBox.Size = new System.Drawing.Size(221, 221);
+            this.collectionBox.TabIndex = 4;
+            this.collectionBox.Text = "";
+            this.collectionBox.TextChanged += new System.EventHandler(this.collectionBox_TextChanged);
+            // 
+            // minSort
+            // 
+            this.minSort.Location = new System.Drawing.Point(12, 284);
+            this.minSort.Name = "minSort";
+            this.minSort.Size = new System.Drawing.Size(75, 23);
+            this.minSort.TabIndex = 5;
+            this.minSort.Text = "Sort-";
+            this.minSort.UseVisualStyleBackColor = true;
+            this.minSort.Click += new System.EventHandler(this.minSort_Click);
+            // 
+            // maxSort
+            // 
+            this.maxSort.Location = new System.Drawing.Point(158, 284);
+            this.maxSort.Name = "maxSort";
+            this.maxSort.Size = new System.Drawing.Size(75, 23);
+            this.maxSort.TabIndex = 6;
+            this.maxSort.Text = "Sort+";
+            this.maxSort.UseVisualStyleBackColor = true;
+            this.maxSort.Click += new System.EventHandler(this.maxSort_Click);
+            // 
+            // linqBox
+            // 
+            this.linqBox.Location = new System.Drawing.Point(239, 46);
+            this.linqBox.Name = "linqBox";
+            this.linqBox.Size = new System.Drawing.Size(221, 221);
+            this.linqBox.TabIndex = 7;
+            this.linqBox.Text = "";
+            // 
+            // query1
+            // 
+            this.query1.AutoSize = true;
+            this.query1.Location = new System.Drawing.Point(274, 13);
+            this.query1.Name = "query1";
+            this.query1.Size = new System.Drawing.Size(75, 23);
+            this.query1.TabIndex = 8;
+            this.query1.Text = "Query1";
+            this.query1.UseVisualStyleBackColor = true;
+            this.query1.Click += new System.EventHandler(this.query1_Click);
+            // 
+            // query2
+            // 
+            this.query2.AutoSize = true;
+            this.query2.Location = new System.Drawing.Point(365, 13);
+            this.query2.Name = "query2";
+            this.query2.Size = new System.Drawing.Size(75, 23);
+            this.query2.TabIndex = 9;
+            this.query2.Text = "Query2";
+            this.query2.UseVisualStyleBackColor = true;
+            this.query2.Click += new System.EventHandler(this.query2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 425);
+            this.Controls.Add(this.query2);
+            this.Controls.Add(this.query1);
+            this.Controls.Add(this.linqBox);
+            this.Controls.Add(this.maxSort);
+            this.Controls.Add(this.minSort);
+            this.Controls.Add(this.collectionBox);
+            this.Controls.Add(this.generate);
+            this.Controls.Add(this.numberOfStuents);
+            this.Controls.Add(this.studentsLabel);
+            this.Name = "Form1";
+            this.Text = "OOP 2-1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label studentsLabel;
+        private System.Windows.Forms.TextBox numberOfStuents;
+        private System.Windows.Forms.Button generate;
+        private System.Windows.Forms.RichTextBox collectionBox;
+        private System.Windows.Forms.Button minSort;
+        private System.Windows.Forms.Button maxSort;
+        private System.Windows.Forms.RichTextBox linqBox;
+        private System.Windows.Forms.Button query1;
+        private System.Windows.Forms.Button query2;
+    }
+}
+
---
+++ Lab1.2/Lab1.2/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1._2
+{
+    public partial class Form1 : Form
+    {
+        List<int> list = new List<int>();
+        public bool trigerSort;
+        Random marksGenerator = new Random();
+        int mark;
+        public delegate int Comparator<in T>(T x, T y);
+        Comparator<int> sortbyu = delegate (int x, int y)
+        {
+            if (x > y) return -1;
+            else if (y > x) return 1;
+            else return 0;
+        };
+        Comparator<int> sortbyv = delegate (int x, int y)
+        {
+            if (x < y) return -1;
+            else if (y < x) return 1;
+            else return 0;
+        };
+        public List<int> sortby(List<int> t, Comparator<int> u)
+        {
+            int temp;
+            for (int j = 0; j < t.Count; j++)
+            {
+                for (int i = 0; i < t.Count - 1; i++)
+                {
+                    if (u(t[i], t[i + 1]) > 0)
+                    {
+                        temp = t[i];
+                        t[i] = t[i + 1];
+                        t[i + 1] = temp;
+                    }
+                }
+            }
+            return t;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.collectionBox.Text = "";
+            list.Clear();
+            for (int i = 0; i < Convert.ToInt16(this.numberOfStuents.Text); i++)
+            {
+                mark = marksGenerator.Next(4, 11);
+                list.Add(mark);
+                this.collectionBox.Text = this.collectionBox.Text + "\n" + (i + 1) + ".Mark: " + mark;
+            }
+        }
+
+        private void minSort_Click(object sender, EventArgs e)
+        {
+            linqBox.Clear();
+            try
+            {
+                if (list.Count == 0) throw new Exception();
+                sortby(list, sortbyv);
+
+                for (int i = 0; i < list.Count; i++)
+                {
+                    linqBox.Text = linqBox.Text + "\n" + (i + 1) + ".Mark: " + list[i];
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка, сначала сгенериуйте коллекцию!");
+            }
+
+        } // сортировка по возрастанию
+
+        private void maxSort_Click(object sender, EventArgs e)
+        {
+            linqBox.Clear();
+            try
+            {
+                if (list.Count == 0) throw new Exception();
+                sortby(list, sortbyu);
+
+                for (int i = 0; i < list.Count; i++)
+                {
+                    linqBox.Text = linqBox.Text +"\n" + (i + 1) + ".Mark: "  + list[i];
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка, сначала сгенериуйте коллекцию!");
+            }
+        } // сортировка по убыванию
+
+        private void collectionBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void query1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            this.linqBox.Text = "";            
+            IEnumerable<int> aMarks = list.Where(n => n>7).Select(n => n);
+            foreach(int mark in aMarks)
+            {
+                this.linqBox.Text = this.linqBox.Text + "\n" + (i + 1) + ".Mark: " + mark;
+                i++;
+            }
+
+        }
+
+        private void query2_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            this.linqBox.Text = "";
+            IEnumerable<int> aMarks = list.Where(n => n == 10).Select(n => n);
+            foreach (int mark in aMarks)
+            {
+                this.linqBox.Text = this.linqBox.Text + "\n" + (i + 1) + ".Mark: " + mark;
+                i++;
+            }
+        }
+
+    }
+}
